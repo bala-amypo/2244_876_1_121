df429a8a201db592ef8365aee72d4ae40d2fb5d0
bala-amypo <bala@amypo.in>
2025-12-19 06:05:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ApiException;
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public Student addStudent(Student student) {
+
+        if (student.getRollNumber() == null || student.getName() == null) {
+            throw new ApiException("missing");
+        }
+
+        if (studentRepository.findByRollNumber(student.getRollNumber()).isPresent()) {
+            throw new ApiException("exists");
+        }
+
+        if (student.getYear() == null || student.getYear() < 1 || student.getYear() > 5) {
+            throw new ApiException("year");
+        }
+
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
