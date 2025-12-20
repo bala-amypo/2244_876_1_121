0940ee9c9bb8350c3f2f9075362f83a993bc737e
bala-amypo <bala@amypo.in>
2025-12-20 10:15:52 +0000

    User commit

--- src/main/java/com/example/demo/controller/ExamSessionController.java
+++ src/main/java/com/example/demo/controller/ExamSessionController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.ExamSession;
+import com.example.demo.model.Student;
 import com.example.demo.service.ExamSessionService;
-
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Set;
+
 @RestController
-@RequestMapping("/sessions")
+@RequestMapping("/api/sessions")
 public class ExamSessionController {
 
     private final ExamSessionService examSessionService;
@@ -15,13 +18,33 @@ public class ExamSessionController {
         this.examSessionService = examSessionService;
     }
 
+   
     @PostMapping
     public ExamSession createSession(@RequestBody ExamSession session) {
         return examSessionService.createSession(session);
     }
 
-    @GetMapping("/{sessionId}")
-    public ExamSession getSession(@PathVariable Long sessionId) {
-        return examSessionService.getSession(sessionId);
+    @GetMapping("/{id}")
+    public ExamSession getSession(@PathVariable Long id) {
+        return examSessionService.getSession(id);
+    }
+
+    @GetMapping
+    public List<ExamSession> getAllSessions() {
+        return examSessionService.getAllSessions();
+    }
+
+    
+    @PostMapping("/{sessionId}/students/{studentId}")
+    public ExamSession addStudentToSession(
+            @PathVariable Long sessionId,
+            @PathVariable Long studentId) {
+
+        return examSessionService.addStudentToSession(sessionId, studentId);
+    }
+
+    @GetMapping("/{sessionId}/students")
+    public Set<Student> getStudentsForSession(@PathVariable Long sessionId) {
+        return examSessionService.getStudentsForSession(sessionId);
     }
 }
