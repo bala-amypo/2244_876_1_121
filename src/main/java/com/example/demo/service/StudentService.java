f492eacc3b9cb6874836253d48da2faa68b86e19
bala-amypo <bala@amypo.in>
2025-12-21 06:07:25 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,7 +1,8 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Student;
 import java.util.List;
+
+import com.example.demo.model.Student;
 
 public interface StudentService {
 
---
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.service.StudentService;
+
+
+import com.example.demo.model.ExamRoom; 
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.exception.ApiException;
+import com.example.demo.model.Student;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+    
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+    
+    public Student addStudent(Student student){
+       
+        return studentRepository.save(student);
+    }
+
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+
+}
