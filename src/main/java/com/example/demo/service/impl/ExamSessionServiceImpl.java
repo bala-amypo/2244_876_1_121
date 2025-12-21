1aca5039a873079bfc82029bfd5cf29f0b1d68f7
bala-amypo <bala@amypo.in>
2025-12-21 06:46:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/ExamSessionServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.demo.service.impl;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.ExamSessionRepository;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.ExamSessionService;
+import com.example.demo.model.ExamSession;
+
+@Service
+public class ExamSessionServiceImpl implements ExamSessionService {
+
+    private final ExamSessionRepository examSessionRepository;
+    private final StudentRepository studentRepository;
+
+    public ExamSessionServiceImpl(ExamSessionRepository examSessionRepository, StudentRepository studentRepository) {
+        this.examSessionRepository = examSessionRepository;
+        this.studentRepository = studentRepository;
+    }
+
+    public ExamSession createSession(ExamSession session) {
+
+        if (session.getExamDate().isBefore(LocalDate.now()))
+            return null;
+
+        if (session.getStudents() == null || session.getStudents().isEmpty())
+            return null;
+
+        return examSessionRepository.save(session);
+    }
+
+    public ExamSession getSession(Long sessionId) {
+        return examSessionRepository.findById(sessionId).orElse(null);
+    }
+}
