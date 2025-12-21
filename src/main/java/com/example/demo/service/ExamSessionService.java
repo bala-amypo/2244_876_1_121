7d9d057bcaa467086986df7fa8d32911844081d3
bala-amypo <bala@amypo.in>
2025-12-21 07:22:31 +0000

    User commit

--- src/main/java/com/example/demo/service/ExamSessionService.java
+++ src/main/java/com/example/demo/service/ExamSessionService.java
@@ -3,6 +3,9 @@ package com.example.demo.service;
 import com.example.demo.model.ExamSession;
 
 public interface ExamSessionService {
+    
     ExamSession createSession(ExamSession session);
+    
     ExamSession getSession(Long sessionId);
+    
 }
--- src/main/java/com/example/demo/service/impl/ExamSessionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ExamSessionServiceImpl.java
@@ -2,17 +2,12 @@ package com.example.demo.service.impl;
 
 import java.time.LocalDate;
 
-
 import org.springframework.stereotype.Service;
 
 import com.example.demo.repository.ExamSessionRepository;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.ExamSessionService;
-
-import com.example.demo.exception.ApiException;
-
 import com.example.demo.model.ExamSession;
-
 
 @Service
 public class ExamSessionServiceImpl implements ExamSessionService{
@@ -26,12 +21,17 @@ public class ExamSessionServiceImpl implements ExamSessionService{
     }
 
     public ExamSession createSession(ExamSession session){
-        
+        if (session.getExamDate().isBefore(LocalDate.now()))
+            return null;
+
+        if (session.getStudents() == null || session.getStudents().isEmpty())
+            return null;
+
         return examSessionRepository.save(session);
     }
 
     public ExamSession getSession(Long sessionId){
-        return examSessionRepository.findById(sessionId).orElseThrow();
+        return examSessionRepository.findById(sessionId).orElse(null);
     }
     
 }
