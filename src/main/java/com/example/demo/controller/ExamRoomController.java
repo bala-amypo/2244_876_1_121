91b88c84b14978e652fb847f884664144a6e61df
bala-amypo <bala@amypo.in>
2025-12-21 06:16:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/ExamRoomController.java
+++ src/main/java/com/example/demo/controller/ExamRoomController.java
@@ -1,28 +1,31 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import com.example.demo.model.ExamRoom;
 import com.example.demo.service.ExamRoomService;
 
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-
 @RestController
 @RequestMapping("/rooms")
-public class ExamRoomController {
+public class ExamRoomController{
 
     private final ExamRoomService examRoomService;
 
     public ExamRoomController(ExamRoomService examRoomService) {
         this.examRoomService = examRoomService;
     }
-
     @PostMapping
-    public ExamRoom addRoom(@RequestBody ExamRoom room) {
-        return examRoomService.addRoom(room);
+    public ResponseEntity<ExamRoom> addRoom(@Valid @RequestBody ExamRoom examRoom){
+        return ResponseEntity.status(201).body(examRoomService.addRoom(examRoom));
     }
 
     @GetMapping
-    public List<ExamRoom> getAllRooms() {
-        return examRoomService.getAllRooms();
+    public ResponseEntity<List<ExamRoom>> listRooms(){
+        return ResponseEntity.status(200).body(examRoomService.getAllRooms());
     }
 }
