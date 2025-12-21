fcaa6dc7e5c49f4ccbcca609c5e93987f3a3c423
bala-amypo <bala@amypo.in>
2025-12-21 14:32:11 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ExamSessionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ExamSessionServiceImpl.java
@@ -1,41 +1,24 @@
-package com.example.demo.service.impl;
-
-import java.time.LocalDate;
-
-
-import org.springframework.stereotype.Service;
-
-import com.example.demo.repository.ExamSessionRepository;
-import com.example.demo.repository.StudentRepository;
-import com.example.demo.service.ExamSessionService;
-
-import com.example.demo.exception.ApiException;
-
-import com.example.demo.model.ExamSession;
-
-
 @Service
-public class ExamSessionServiceImpl implements ExamSessionService{
+public class ExamSessionServiceImpl implements ExamSessionService {
 
     private final ExamSessionRepository examSessionRepository;
-    private final StudentRepository studentRepository;
 
-    public ExamSessionServiceImpl(ExamSessionRepository examSessionRepository, StudentRepository studentRepository) {
+    public ExamSessionServiceImpl(ExamSessionRepository examSessionRepository) {
         this.examSessionRepository = examSessionRepository;
-        this.studentRepository = studentRepository;
     }
 
-    public ExamSession createSession(ExamSession session){
-        if (session.getExamDate().isBefore(LocalDate.now()))
-            throw new ApiException("past");
+    @Override
+    public ExamSession createSession(ExamSession session) {
 
-        if (session.getStudents() == null || session.getStudents().isEmpty())
-            throw new ApiException("at least 1 student");
+        if (session.getExamDate() == null) return null;
+        if (session.getExamDate().isBefore(LocalDate.now())) return null;
+        if (session.getStudents() == null || session.getStudents().isEmpty()) return null;
+
         return examSessionRepository.save(session);
     }
 
-    public ExamSession getSession(Long sessionId){
-        return examSessionRepository.findById(sessionId).orElseThrow(()->new ApiException("Session not found"));
+    @Override
+    public ExamSession getSession(Long sessionId) {
+        return examSessionRepository.findById(sessionId).orElse(null);
     }
-    
 }
