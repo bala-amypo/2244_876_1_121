f45e5e67ad4d4c8962269978e5554f478f67d1a3
bala-amypo <bala@amypo.in>
2025-12-26 11:27:41 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -19,12 +21,38 @@ public class OpenApiConfig {
                 .scheme("bearer")
                 .bearerFormat("JWT");
 
+        Server server = new Server()
+                .url("https://9134.32procr.amypo.ai/")
+                .description("Production Server");
+
         return new OpenAPI()
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                )
                 .components(
                         new Components().addSecuritySchemes("bearerAuth", bearerAuth)
                 )
-                .addSecurityItem(
-                        new SecurityRequirement().addList("bearerAuth")
-                );
+                .servers(List.of(server));
     }
 }
+
+// package com.example.demo.config;
+
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.servers.Server;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import java.util.List;
+
+// @Configuration
+// public class OpenApiConfig {
+
+//     @Bean
+//     public OpenAPI customOpenAPI() {
+//         return new OpenAPI()
+//                 // You need to change the port as per your server
+//                 .servers(List.of(
+//                         new Server().url("https://9134.32procr.amypo.ai/")
+//                 ));
+//         }
+// }
